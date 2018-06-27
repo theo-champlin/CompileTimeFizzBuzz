fd7b53705942ffb0680c8b113dd1af8aae94ee0f
theo-champlin <40192128+theo-champlin@users.noreply.github.com>
2018-06-27 16:05:42 -0700

    Cleaning up whitespace
    Making the spacing more consistent, no functionality changes.

--- Second Pass/FizzBuzz.hpp
+++ Second Pass/FizzBuzz.hpp
@@ -4,41 +4,41 @@
 template<unsigned value>
 constexpr auto FizzBuzzConvert()
 {
-    if constexpr (!(value % 5) && !(value % 3))
-    {
-        return constant_expression::literal_to_string("FizzBuzz");
-    }
-    else if constexpr (!(value % 3))
-    {
-        return constant_expression::literal_to_string("Fizz");
-    }
-    else if constexpr (!(value % 5))
-    {
-        return constant_expression::literal_to_string("Buzz");
-    }
-    else
-    {
-        return constant_expression::number_to_string<value>::convert();
-    }
+   if constexpr (!(value % 5) && !(value % 3))
+   {
+      return constant_expression::literal_to_string("FizzBuzz");
+   }
+   else if constexpr (!(value % 3))
+   {
+      return constant_expression::literal_to_string("Fizz");
+   }
+   else if constexpr (!(value % 5))
+   {
+      return constant_expression::literal_to_string("Buzz");
+   }
+   else
+   {
+      return constant_expression::number_to_string<value>::convert();
+   }
 }
 
 template<unsigned value>
 constexpr auto FizzBuzz()
 {
-    constexpr auto current = FizzBuzz<value - 1>();
-    constexpr auto next = FizzBuzzConvert<value>();
-    
-    constant_expression::string<std::size(current) + std::size(next) + 1> aggregated{ 0 };
-    
-    aggregated.copy(std::begin(current), std::end(current));
-    aggregated[std::size(current)] = '\n';
-    aggregated.copy(std::begin(next), std::end(next), std::size(current) + 1);
-    
-    return aggregated;
+   constexpr auto current = FizzBuzz<value - 1>();
+   constexpr auto next = FizzBuzzConvert<value>();
+
+   constant_expression::string<std::size(current) + std::size(next) + 1> aggregated{ 0 };
+
+   aggregated.copy(std::begin(current), std::end(current));
+   aggregated[std::size(current)] = '\n';
+   aggregated.copy(std::begin(next), std::end(next), std::size(current) + 1);
+
+   return aggregated;
 }
 
 template<>
 constexpr auto FizzBuzz<1>()
 {
-    return FizzBuzzConvert<1>();
+   return FizzBuzzConvert<1>();
 }
